445226e624633d1b8dbe4998013fc1c9a47aedab
Dominique DEBERT <dominiquedebert@gmail.com>
2026-01-09 06:43:00 +0100

    Fix playback: remove audio crossOrigin to support Jellyfin streams without CORS

--- src/pages/ItemDetail/MusicDetail.tsx
+++ src/pages/ItemDetail/MusicDetail.tsx
@@ -446,7 +446,6 @@ export default function MusicDetail() {
         {/* Hidden Audio Element */}
         <audio
           ref={audioRef}
-          crossOrigin="anonymous"
           onTimeUpdate={handleTimeUpdate}
           onLoadedMetadata={handleLoadedMetadata}
           onPlay={handlePlay}
